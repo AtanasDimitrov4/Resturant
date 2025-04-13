305fea334ad8ae902d12e3397389c6a86595e5f9
Atanas Dimitrov4 <ataldi@abv.bg>
2025-04-13 15:36:01 +0300

    create App,jsx without router and provider

--- client/Resturant/src/App.jsx
+++ client/Resturant/src/App.jsx
@@ -1,12 +1,36 @@
-import RestaurantMenu from "./components/ResturnatMenu"; 
+import { useState, useEffect } from "react";
+import DishCard from "./components/DishCard";
 
-function App() {
+export default function App() {
+  const [dishes, setDishes] = useState([]);
+
+  useEffect(() => {
+    fetch("plates.json")
+    .then((result) => result.json())
+    .then((data)) => setDishes(data))
+    .catch((err)) => console.error("Faild to fetch plates", err));
+  }, []);
+
   return (
-    <div>
-      <RestaurantMenu />
-    </div>
+    <div className="app" >
+      <header className="header" />
+        <h1>Delicius Resturant</h1>
+        <p>No Jackal, just good food</p>
+        </header>
+
+        <main>
+          <h2>Menu</h2>
+          <div className="dish-grid">
+            {dish.map((dish) => (
+              <DishCard key={dish.id} dish={dish}/>
+
+            ))}
+            </div>
+        </main>
+
+        <footer className="footer">
+          <p>@ 2025 AlbertoPer</p>
+        </footer>
+        </div>
   );
 }
-
-export default App;
-
